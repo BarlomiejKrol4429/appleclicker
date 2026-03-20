138f5dcec6613da3c2fd093a11268695b450db29
BarlomiejKrol4429 <bartlomiej.krol@uczen.mechaniktg.pl>
2026-03-20 12:05:42 +0100

    layout + WIP funkcjonalnosc

--- app/src/main/java/com/example/appleclicker/MainActivity.java
+++ app/src/main/java/com/example/appleclicker/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.appleclicker;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button start;
+
+    ImageView japuszko1, japuszko2, japuszko3, japuszko4, japuszko5, japuszko6, japuszko7, japuszko8, japuszko9;
+    private int ileSekund = 10;
+    private CountDownTimer countDownTimer;
+    private boolean czyGraTrwa = false;
+
+    public void rozpocznijGre(){
+        if(!czyGraTrwa){
+            uruchomZegar();
+            czyGraTrwa = true;
+        }
+    }
+
+    private void zatrzymajZegar(){
+        countDownTimer.cancel();
+    }
+
+    private void uruchomZegar(){
+        countDownTimer = new CountDownTimer(ileSekund * 1000, 1000) {
+            @Override
+            public void onFinish() {
+
+            }
+
+            @Override
+            public void onTick(long l) {
+                ileSekund = (int) (l/1000);
+                button.setText(ileSekund+"");
+            }
+        };
+        countDownTimer.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +58,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        start = findViewById(R.id.start);
+
+        start.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        rozpocznijGre();
+                    }
+                }
+        );
     }
 }
