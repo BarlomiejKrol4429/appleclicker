bfe0abda9903d6cd4baf5827e0d37d53fdcc593f
BarlomiejKrol4429 <bartlomiej.krol@uczen.mechaniktg.pl>
2026-03-27 10:47:35 +0100

    timer

--- app/src/main/java/com/example/appleclicker/MainActivity.java
+++ app/src/main/java/com/example/appleclicker/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,38 +14,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     Button start;
     TextView czas, punkty;
-    ImageView japuszko1, japuszko2, japuszko3, japuszko4, japuszko5, japuszko6, japuszko7, japuszko8, japuszko9;
-    private int ileSekund = 10;
+    ImageView j1, j2, j3, j4, j5, j6, j7, j8, j9;
+    private int ileSekund;
     private CountDownTimer countDownTimer;
     private boolean czyGraTrwa = false;
 
     public void rozpocznijGre(){
-        if(!czyGraTrwa){
-
-            uruchomZegar();
-            czyGraTrwa = true;
-        }
+        ileSekund = 5;
+        uruchomZegar();
+        start.setEnabled(false);
+        losujJapka();
     }
 
     private void uruchomZegar(){
+        czas = findViewById(R.id.czas);
+
         countDownTimer = new CountDownTimer(ileSekund * 1000, 1000) {
             @Override
             public void onFinish() {
-
+                start.setEnabled(true);
+                czas.setText(0+"");
             }
 
             @Override
             public void onTick(long l) {
                 ileSekund = (int) (l/1000);
-                czas.setText(ileSekund+"");
+                czas.setText((ileSekund+1)+"");
             }
         };
         countDownTimer.start();
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,13 +65,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
         start = findViewById(R.id.start);
+        j1 = findViewById(R.id.japuszko1);
+        j2 = findViewById(R.id.japuszko2);
+        j3 = findViewById(R.id.japuszko3);
+        j4 = findViewById(R.id.japuszko4);
+        j5 = findViewById(R.id.japuszko5);
+        j6 = findViewById(R.id.japuszko6);
+        j7 = findViewById(R.id.japuszko7);
+        j8 = findViewById(R.id.japuszko8);
+        j9 = findViewById(R.id.japuszko9);
 
         start.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        j1.setImageResource(R.drawable.brakjapuszek);
+                        j2.setImageResource(R.drawable.brakjapuszek);
+                        j3.setImageResource(R.drawable.brakjapuszek);
+                        j4.setImageResource(R.drawable.brakjapuszek);
+                        j5.setImageResource(R.drawable.brakjapuszek);
+                        j6.setImageResource(R.drawable.brakjapuszek);
+                        j7.setImageResource(R.drawable.brakjapuszek);
+                        j8.setImageResource(R.drawable.brakjapuszek);
+                        j9.setImageResource(R.drawable.brakjapuszek);
                         rozpocznijGre();
-                        japuszko1.setImageIcon();
                     }
                 }
         );
